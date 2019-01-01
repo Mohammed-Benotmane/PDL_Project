96c93bb74394bc51652ba130be9c5a8284e17605
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-01-01 22:57:25 +0100

    add modifier function

--- src/com/company/Bibliotheque.java
+++ src/com/company/Bibliotheque.java
@@ -7,10 +7,14 @@ static ArrayList <Document> disponible = new ArrayList<>();
 static ArrayList <Document> emprunter = new ArrayList<>();
 
 public void modifier(Document document,int nombreDeDocument){
-   /* for(int i=0;i<disponible.size();i++){
+    int temp;
+    for(int i=0;i<disponible.size();i++){
         if(disponible.get(i).equals(document)){
-            disponible.get(i)
+            temp= disponible.get(i).getNombreExemplaire()+nombreDeDocument;
+            disponible.get(i).setNombreExemplaire(temp);
+            System.out.println("le nombre d'exemplaire a bien été modifier");
+            break;
         }
-    }*/
+    }
 }
 }
