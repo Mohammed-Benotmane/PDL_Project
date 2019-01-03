709f3b2c4075e153c00f3736276db5d643ce572d
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-01-03 21:48:23 +0100

    add consulter function

--- src/com/company/Bibliotheque.java
+++ src/com/company/Bibliotheque.java
@@ -3,18 +3,24 @@ package com.company;
 import java.util.ArrayList;
 
 public class Bibliotheque {
-static ArrayList <Document> disponible = new ArrayList<>();
-static ArrayList <Document> emprunter = new ArrayList<>();
+    static ArrayList<Document> disponible = new ArrayList<>();
+    static ArrayList<Document> emprunter = new ArrayList<>();
 
-public void modifier(Document document,int nombreDeDocument){
-    int temp;
-    for(int i=0;i<disponible.size();i++){
-        if(disponible.get(i).equals(document)){
-            temp= disponible.get(i).getNombreExemplaire()+nombreDeDocument;
-            disponible.get(i).setNombreExemplaire(temp);
-            System.out.println("le nombre d'exemplaire a bien été modifier");
-            break;
+    public void modifier(Document document, int nombreDeDocument) {
+        int temp;
+        for (int i = 0; i < disponible.size(); i++) {
+            if (disponible.get(i).equals(document)) {
+                temp = disponible.get(i).getNombreExemplaire() + nombreDeDocument;
+                disponible.get(i).setNombreExemplaire(temp);
+                System.out.println("le nombre d'exemplaire a bien été modifier");
+                break;
+            }
         }
     }
-}
+
+    public void consulter() {
+        for (int j = 0; j < disponible.size(); j++) {
+            System.out.println("1-" + disponible.get(j).getTitre());
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Bibliotheque bibliotheque = new Bibliotheque();
+
 
     }
 }
